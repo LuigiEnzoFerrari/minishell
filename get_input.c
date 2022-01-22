5e0a9908d30b686caa18c30f2216cd8d3bccc05c
rafa <rafa>
2022-01-22 20:57:29 -0300

    refactor : get_input

--- get_input.c
+++ get_input.c
@@ -3,20 +3,15 @@
 void	save_history(char *input)
 {
 	if (input == NULL || *input == '\0')
-        return ;
-    add_history(input);
+		return ;
+	add_history(input);
 }
 
-
-char *get_full_prompt(t_environ *envs)
+char	*get_full_prompt(t_environ *envs)
 {
-	// char		path[4096];
 	char		*path_colored;
 	char		*path;
 
-	// change the getcwd function to ta $PWD variable
-
-	// getcwd(path, sizeof(path));
 	path = get_env_value(envs, "PWD");
 	path_colored = ft_strjoin(IGREEN, path);
 	path_colored = ft_rejoin(path_colored, COLOR_OFF);
@@ -26,16 +21,17 @@ char *get_full_prompt(t_environ *envs)
 
 char	*get_input(t_environ *envs)
 {
-	char	*buff;
+	char	*old_input_address;
+	char	*prompt;
 	char	*input;
 
-	buff = get_full_prompt(envs);
-	input = readline(buff);
-    free(buff);
-	buff = input;
+	prompt = get_full_prompt(envs);
+	input = readline(prompt);
+	old_input_address = input;
 	input = ft_skip_chr(input, ft_isblank, 1);
 	input = ft_strdup(input);
 	save_history(input);
-	free(buff);
+	free(old_input_address);
+	free(prompt);
 	return (input);
 }
