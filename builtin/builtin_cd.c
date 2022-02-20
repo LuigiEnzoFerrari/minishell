a05706e850ea343e6ecd369e2709e83415f3d058
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2022-02-20 05:47:23 -0300

    fix: cd bug
    fix the error when the user just type cd with no arguments, add a new prompt error in case the user put more the one argument.

--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -1,6 +1,6 @@
 # include <minishell.h>
 
-static void	new_path(char **args, t_environ *envs, char *path)
+static char	*new_path(t_environ *envs, char *path)
 {
 	char	*new;
 
@@ -8,9 +8,7 @@ static void	new_path(char **args, t_environ *envs, char *path)
 		new = ft_strdup(get_env_value(envs, path));
 	else
 		new = ft_strdup(path);
-    if (*args != NULL)
-        free(*args);
-	*args = new;
+    return (new);
 }
 
 // bash: cd: in: No such file or directory  
@@ -18,19 +16,25 @@ void	builtin_cd(char **args, t_environ *envs)
 {
 	char	*path;
 
-	if (*args == NULL || ft_strcmp(*args, "~") == 0)
-		new_path(args, envs, "HOME");
+    if (*args != NULL && *(args + 1) != NULL)
+    {
+        ft_putendl_fd("bash: too many arguments", STDERR_FILENO);
+        return ;
+    }
+	else if (*args == NULL || ft_strcmp(*args, "~") == 0)
+		path = new_path(envs, "HOME");
 	else if (ft_strcmp(*args, "-") == 0)
-		new_path(args, envs, "OLDPWD");
+		path = new_path(envs, "OLDPWD");
 	else
-		new_path(args, NULL, *args);
-    if (chdir(*args) != 0)
+		path = new_path(NULL, *args);
+    if (chdir(path) != 0)
 		perror(strerror(errno));
 	else
     {
+        free(path);
 		path = getcwd(NULL, 0);
 		update_env(envs, "OLDPWD", get_env_value(envs, "PWD"));
 		update_env(envs, "PWD", path);
-		free(path);
 	}
+    free(path);
 }
