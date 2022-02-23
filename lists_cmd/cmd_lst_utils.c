2cf501c5fbd3de82b4bae643ee56e4d4e6bd38d6
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2022-02-23 01:13:03 -0300

    refactor: new way to create args on cmd_lst_utils.c
    Need be more tested.

--- lists_cmd/cmd_lst_utils.c
+++ lists_cmd/cmd_lst_utils.c
@@ -13,10 +13,31 @@ size_t	count(t_tokens *tokens)
 	return (i);
 }
 
-void    alloc_args_labels(t_tokens *tokens, char ***args, int **labels, size_t n)
+void	alloc_args_labels(t_tokens *tokens, char ***args, int **labels, size_t n)
 {
 	*args = malloc(sizeof(char *) * n);
 	*labels = malloc(sizeof(int) * n);
+}
+
+void	create_cmd(t_tokens **tokens, char **args, int *labels)
+{
+	t_tokens *holder;
+	size_t	i;
+
+	i = 0;
+	holder = (*tokens);
+	while (holder != NULL && holder->label != PIPE)
+	{
+		args[i] = ft_strdup(holder->token);
+		labels[i] = holder->label;
+		i++;
+		holder = holder->next;
+	}
+	args[i] = NULL;
+	labels[i] = 0;
+	while (holder != NULL && holder->label == PIPE)
+		holder = holder->next;
+	(*tokens) = holder;
 }
 
 t_cmds	*pipe_commands(t_tokens *tokens)
@@ -28,34 +49,11 @@ t_cmds	*pipe_commands(t_tokens *tokens)
 
 	i = 0;
 	cmds = NULL;
-	args = malloc(sizeof(char *) * ((count(tokens) + 1) + 1));
-	labels = malloc(sizeof(int) * ((count(tokens) + 1) + 1));
-    // alloc_args_labels(tokens, &args, &labels, ((count(tokens) + 1) + 1));
 	while (tokens != NULL)
 	{
-		args[i] = ft_strdup(tokens->token);
-		labels[i] = tokens->label;
-		tokens = tokens->next;
-		if (tokens == NULL || tokens->label == PIPE)
-		{
-			args[i + 1] = NULL;
-			labels[i + 1] = 0;
-			push_cmd(&cmds, args, labels);
-			if (tokens != NULL)
-			{
-				while (tokens->next != NULL && tokens->next->label == PIPE)
-					tokens = tokens->next;
-				if (tokens->next != NULL)
-				{
-					args = malloc(sizeof(char *) * (count(tokens->next) + 1));
-					labels = malloc(sizeof(int) * (count(tokens->next) + 1));
-				}
-				tokens = tokens->next;
-			}
-			i = 0;
-		}
-		else
-			i++;
+		alloc_args_labels(tokens, &args, &labels, ((count(tokens) + 1) + 1));
+		create_cmd(&tokens, args, labels);
+		push_cmd(&cmds, args, labels);
 	}
 	return (cmds);
 }
