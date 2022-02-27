4ce9b726d2133a2fa27ea9347ae684c5b7374787
rafa <rafa>
2022-02-27 19:43:41 +0100

    add header to files

--- steps/parse_input/lexical_analysis_and_parse.c
+++ steps/parse_input/lexical_analysis_and_parse.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexical_analysis_and_parse.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 19:40:06 by lenzo-pe          #+#    #+#             */
+/*   Updated: 2022/02/27 19:40:07 by lenzo-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minishell.h>
 
 t_tokens	*lexical_analysis_and_parse(char *input, t_env_vars *vars)
